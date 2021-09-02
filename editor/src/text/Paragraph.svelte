4ae5cccf6ec304dc6dd4ac839c69fbf5849fb50b
steven <github@stevenwaterman.uk>
2021-09-02 11:03:08 +0100

    Fix the selection in between sections

--- editor/src/text/Paragraph.svelte
+++ editor/src/text/Paragraph.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+import { playingStore } from "../audio/audio";
+
   import Section from "./Section.svelte";
   import type { ParagraphStore } from "./textState";
 
@@ -11,9 +13,13 @@
     margin-block-end: 0;
     padding-block-end: 1em;
   }
+
+  .paragraph.nonePlaying::selection {
+    background-color: rgba(76, 108, 169, 0.99);
+  }
 </style>
 
-<p class="paragraph">
+<p class="paragraph" class:nonePlaying={!$playingStore}>
   {#each $paragraphStore as sectionStore, idx}
     <Section sectionStore={ sectionStore } last={idx === $paragraphStore.length - 1}/>
   {/each}
