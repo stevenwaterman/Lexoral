a53f95cc5811ab98e6d10a047d6e8756758023ad
steven <github@stevenwaterman.uk>
2021-09-17 14:26:31 +0100

    Rework fetch cloud function based on firebase example code

--- controller/src/Home.svelte
+++ controller/src/Home.svelte
@@ -2,6 +2,18 @@
   import type { User } from "firebase/auth";
 
   export let user: User | null;
+
+  async function sendRequest() {
+    if (user === null) return;
+
+    const idToken = await user.getIdToken();
+    fetch("https://europe-west2-lexoral-test.cloudfunctions.net/fetch", {
+      method: "get",
+      headers: {
+        "Authorization": `Bearer ${idToken}`
+      }
+    })
+  }
 </script>
 
 <style>
@@ -12,4 +24,5 @@
   Welcome to Lexoral
 {:else}
   Welcome, {user.email}
+  <button on:click={sendRequest}>Fetch</button>
 {/if}
--- functions/fetch/src/index.ts
+++ functions/fetch/src/index.ts
@@ -1,40 +1,64 @@
 import { Storage } from "@google-cloud/storage";
-import type { Request, Response } from "express";
-import * as admin from "firebase-admin";
+import type { NextFunction, Request, Response } from "express";
+import admin from "firebase-admin";
+import functions from "firebase-functions";
+import corsFactory from "cors";
+import express from "express";
 
-export function run(req: Request, res: Response) {
-  console.log("invoked")
+type HydratedRequest = Request & { user?: admin.auth.DecodedIdToken };
 
-  const authorization = req.headers.authorization;
-  if (authorization === undefined) {
-    res.sendStatus(401);
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+async function validateFirebaseIdToken(
+  req: HydratedRequest, 
+  res: Response,
+  next: NextFunction
+) {
+  functions.logger.log('Check if request is authorized with Firebase ID token');
+
+  if ((!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) &&
+      !(req.cookies && req.cookies.__session)) {
+    functions.logger.error(
+      'No Firebase ID token was passed as a Bearer token in the Authorization header.',
+      'Make sure you authorize your request by providing the following HTTP header:',
+      'Authorization: Bearer <Firebase ID Token>',
+      'or by passing a "__session" cookie.'
+    );
+    res.status(403).send('Unauthorized');
     return;
   }
-  if (!authorization.startsWith("Bearer ")) {
-    res.sendStatus(400);
-    return;
-  }
-  const token = authorization.split("Bearer ")[1];
-  if (token === undefined) {
-    res.sendStatus(400);
-    return;
-  }
-  
-  admin
-    .initializeApp()
-    .auth()
-    .verifyIdToken(token)
-    .then(decodedToken => {
-      console.log(decodedToken);
-      sendFile(res);
-    })
-    .catch(error => {
-      console.log(error);
-      res.sendStatus(403);
-    })
-}
 
-async function sendFile(res: Response) {
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    functions.logger.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else if(req.cookies) {
+    functions.logger.log('Found "__session" cookie');
+    // Read the ID Token from cookie.
+    idToken = req.cookies.__session;
+  } else {
+    // No cookie
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  try {
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+    functions.logger.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+    return;
+  } catch (error) {
+    functions.logger.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+    return;
+  }
+};
+
+function sendFile(res: Response) {
   res.writeHead(200, {
     "Content-Type": "text/json"
   });
@@ -47,3 +71,14 @@ async function sendFile(res: Response) {
     .on("end", () => {})
     .pipe(res);
 }
+
+function handleRequest(req: HydratedRequest, res: Response) {
+  sendFile(res);
+}
+
+admin.initializeApp();
+const cors = corsFactory({ origin: true });
+const app = express().use(cors).use(validateFirebaseIdToken);
+app.get("/fetch", handleRequest);
+
+export const run = functions.https.onRequest(app);
