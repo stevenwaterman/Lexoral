558be32b47460246f01d2356a33cd03697bc3e71
steven <github@stevenwaterman.uk>
2021-09-22 17:49:13 +0100

    Remove trigger_http from those that don't need it

--- terraform/modules/signupFunction/main.tf
+++ terraform/modules/signupFunction/main.tf
@@ -17,7 +17,6 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb   = 128
   source_archive_bucket = var.bucket
   source_archive_object = google_storage_bucket_object.function_src.name
-  trigger_http          = false
   entry_point           = "run"
   environment_variables = {
     PROJECT_ID = var.project_id
