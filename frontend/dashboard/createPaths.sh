f635047cb67754f41050553db24b7d8e8a792531
steven <github@stevenwaterman.uk>
2021-09-25 16:08:28 +0100

    Fix 404 with mega janky script

---
+++ frontend/dashboard/createPaths.sh
@@ -0,0 +1,7 @@
+echo "Initiating jank"
+for path in /auth/login /auth/signup /auth/verify; do
+  mkdir -p public/$path
+  cp public/index.html public/$path/index.html
+  echo $path
+done
+echo "Jank complete"
