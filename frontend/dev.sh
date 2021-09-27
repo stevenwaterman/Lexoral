2faaea81ee975eb147a131711deec670ba8b279e
steven <github@stevenwaterman.uk>
2021-09-27 13:13:13 +0100

    Simplify deployment of prod while keeping staging disabled

--- frontend/dev.sh
+++ frontend/dev.sh
@@ -1,5 +1,4 @@
-export PROJECT_ID="lexoral-stage"
-export FIREBASE_API_KEY="AIzaSyBxFFOSL7yfPksJMK1drBBabOWlOWLwDE4"
+export BRANCH="stage"
 ( cd dashboard; npm i; npm run dev ) &
 ( cd editor; npm i; npm run dev ) &
 serve -n -l 5000 public
