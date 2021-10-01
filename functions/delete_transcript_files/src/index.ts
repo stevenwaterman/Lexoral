f3a11e2f2d522975f1fcafab12ef71cfc99ab892
steven <github@stevenwaterman.uk>
2021-10-01 17:46:23 +0100

    update delete transcript files to actually delete the files

--- functions/delete_transcript_files/src/index.ts
+++ functions/delete_transcript_files/src/index.ts
@@ -1,12 +1,15 @@
 import utils from "lexoral-utils";
 import { Storage } from "@google-cloud/storage";
 
-export async function run(event: any, context: any) {
-  const resource = context.resource;
-  // log out the resource string that triggered the function
-  console.log('Function triggered by change to: ' +  resource);
-  // now log the full event object
-  console.log(JSON.stringify(event));
+type Context = {
+  params: {
+    transcriptId: string;
+    userId: string;
+  }
+};
+export async function run(event: any, context: Context) {
+  const { transcriptId, userId } = context.params;
+  await utils.storage.deleteFromAll(storage, userId, transcriptId);
 }
 
 const storage = new Storage();
