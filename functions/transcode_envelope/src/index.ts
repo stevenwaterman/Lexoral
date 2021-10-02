035ce80c45c0caa74f31fdca7bf39d7edb3e3bcd
steven <github@stevenwaterman.uk>
2021-10-02 11:26:21 +0100

    Use ffprobe to read metadata

--- functions/transcode_envelope/src/index.ts
+++ functions/transcode_envelope/src/index.ts
@@ -10,19 +10,20 @@ async function handleRequest(req: Request, res: Response) {
 
   const sourceBucket = storage.bucket(`${process.env["PROJECT_ID"]}-raw-audio`);
   const sourceFile = sourceBucket.file(filename);
-  const audio = await transcodeEnvelope(storage, filename, sourceFile);
+  const durationPromise = transcodeEnvelope(storage, filename, sourceFile);
+  const metadataPromise = getMetadata(sourceFile);
+
+  const [duration, metadata] = await Promise.all([durationPromise, metadataPromise]);
+  const audio = {
+    duration,
+    ...metadata
+  };
   await transcript.doc.update({ audio });
-  res.sendStatus(201);
+
+  res.sendStatus(201);  
 }
 
-type EnvelopeMetadata = {
-  duration: number;
-  format: string;
-  channels: number;
-  sampleRate: number;
-}
-
-async function transcodeEnvelope(storage: Storage, name: string, sourceFile: File): Promise<EnvelopeMetadata> {
+async function transcodeEnvelope(storage: Storage, name: string, sourceFile: File): Promise<number> {
   const envelopeBucket = storage.bucket(`${process.env["PROJECT_ID"]}-envelope-audio`);
   const envelopeFile = envelopeBucket.file(`${name}.pcm`);
   const envelope = envelopeFile.createWriteStream();
@@ -35,20 +36,14 @@ async function transcodeEnvelope(storage: Storage, name: string, sourceFile: Fil
       .audioFilter("aresample=1000")
       .format("s16le")
       .output(envelope, {end: true})
-      .on("end", (_, stderr: string) => {
-        const lines = stderr.split("\n");
-        const duration = getDuration(lines);
-        const { format, sampleRate, channels } = getMetadata(lines);
-        resolve({ duration, format, sampleRate, channels })
-      })
+      .on("end", (_, stderr: string) => resolve(getDuration(stderr.split("\n"))))
       .run()
   });
 }
 
 function getDuration(lines: string[]): number {
-  const sizeLines = lines.filter(line => line.trimStart().startsWith("size="))
-  const sizeLine = sizeLines[sizeLines.length - 1];
-  if (sizeLine === undefined) throw new Error("No size lines in ffmpeg output: " + lines);
+  const sizeLine = lines.find(line => line.trimStart().startsWith("size="))
+  if (sizeLine === undefined) throw new Error("No size lines in ffmpeg output: " + JSON.stringify(lines));
 
   const regexResult = /time=[^ ]+/.exec(sizeLine)
   if (regexResult === null) throw new Error("No time found in ffmpeg size line: " + sizeLine);
@@ -65,27 +60,31 @@ function getDuration(lines: string[]): number {
   return rounded;
 }
 
-const channelMap: Record<string, number | undefined> = {
-  mono: 1,
-  stereo: 2
-}
-
-function getMetadata(lines: string[]): {
+function getMetadata(sourceFile: File): Promise<{
   format: string,
   sampleRate: number,
   channels: number
-} {
-  // eg: Stream #0:0: Audio: mp3, 44100 Hz, mono, s16p, 100 kb/s
-  const streamLines = lines.filter(line => line.trimStart().startsWith("Stream #0:0: Audio: "))
-  const streamLine = streamLines[0];
-  if (streamLine === undefined) throw new Error("No stream lines found in ffmpeg output: " + lines);
-  const [streamString, sampleRateHz, channelString] = streamLine.split(", ");
-  const [_, format] = streamString.split("Audio: ");
-  const [sampleRateStr] = sampleRateHz.split(" ");
-  const sampleRate = parseInt(sampleRateStr);
-  const channels: number | undefined = channelMap[channelString];
-  if (channels === undefined) throw new Error("Unrecognised channel layout: " + channelString);
-  return { format, sampleRate, channels };
+}> {
+  return new Promise(resolve => {
+    ffmpeg(sourceFile.createReadStream())
+      .ffprobe((err, data) => {
+        if (err) throw new Error("FFProbe error: " + err);
+
+        const format = data.format.format_name;
+        if (format === undefined) throw new Error("Format not found: " + JSON.stringify(data));
+
+        const stream = data.streams[0];
+        if (stream === undefined) throw new Error("Stream not found: " + JSON.stringify(data));
+
+        const sampleRate = stream.sample_rate;
+        if (sampleRate === undefined) throw new Error("Sample Rate not found: " + JSON.stringify(data));
+
+        const channels = stream.channels;
+        if (channels === undefined) throw new Error("Channels not found: " + JSON.stringify(data));
+
+        resolve({ format, sampleRate, channels });
+      });
+  });
 }
 
 const store = admin.initializeApp().firestore();
