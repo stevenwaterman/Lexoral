85b42b7c6dfcb94c430941be7a7d55a32b6a2696
steven <github@stevenwaterman.uk>
2021-10-24 11:07:27 +0100

    Fix wordlist

--- frontend/editor/src/state/wordStore.ts
+++ frontend/editor/src/state/wordStore.ts
@@ -1,58 +1,14 @@
 let wordSet: Set<string>;
 
 export async function initWords() {
-  return fetch("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
+  return fetch("/assets/wordlist.txt")
     .then(res => res.text())
     .then(text => text.split("\n"))
-    .then(words => words.filter(word => validWord(word)))
-    .then(words => wordSet = new Set<string>(words))
-    .then(() => {
-      [
-        "a", "of", "to", "in", "it", "is", 
-        "be", "as", "at", "so", "we", "he", 
-        "by", "or", "on", "do", "if", "me", 
-        "my", "up", "an", "go", "no", "us", 
-        "am"
-      ].forEach(word => wordSet.add(word))
-    })
-}
-
-function validWord(word: string): boolean {
-  const bannedCharsOnly = word.replace(/[^A-Z0-9.&]/g, "");
-  if (bannedCharsOnly.length > 0) return false;
-
-  const last = word[word.length - 1];
-  if (last === "-") return false;
-
-  return true;
+    .then(words => wordSet = new Set<string>(words));
 }
 
 export function isKnownWord(word: string): boolean {
-  debugger
-  if (wordSet.has(word)) return true;
-
-  // Avoid doing this if possible, and cache the result
-  const alphanumeric = word.replace("[^a-z]", "");
-  if (wordSet.has(alphanumeric)) {
-    wordSet.add(word);
-    return true;
-  } else {
-    return false;
-  };
-}
-
-export function sanitisePlaceholder(placeholder: string): string {
-  const text = placeholder.toLowerCase();
-  const words = text.split(" ");
-  const capitalisedWords = words.map(word => capitaliseUnknownWord(word));
-  return capitalisedWords.join(" ");
-}
-
-function capitaliseUnknownWord(word: string): string {
-  if (word.length === 0) return word;
-  if (isKnownWord(word)) return word;
-
-  return capitalise(word);
+  return wordSet.has(word);
 }
 
 export function capitalise(word: string): string {
