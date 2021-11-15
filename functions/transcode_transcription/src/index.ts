ba894710eec14f723d6fd753973acb2a2b8d3b4c
steven <github@stevenwaterman.uk>
2021-11-15 19:04:32 +0000

    Try and rework the transcode transcription code so that it works

--- functions/transcode_transcription/src/index.ts
+++ functions/transcode_transcription/src/index.ts
@@ -1,5 +1,5 @@
 import ffmpeg from "fluent-ffmpeg";
-import { File, Storage } from "@google-cloud/storage";
+import { Storage } from "@google-cloud/storage";
 import { Request, Response } from "express";
 import admin from "firebase-admin";
 import utils from "lexoral-utils";
@@ -8,25 +8,25 @@ async function handleRequest(req: Request, res: Response) {
   const { user, transcript } = await utils.userTranscript.getAll(req, res, store);
   const filename = `${user.id}_${transcript.id}`
 
-  const sourceBucket = storage.bucket(`${process.env["PROJECT_ID"]}-raw-audio`);
-  const sourceFile = sourceBucket.file(filename);
-  await transcodePlayback(storage, filename, sourceFile);
+  await transcodePlayback(storage, filename);
 
   res.sendStatus(201);  
 }
 
-async function transcodePlayback(storage: Storage, name: string, sourceFile: File): Promise<void> {
-  const transcriptionBucket = storage.bucket(`${process.env["PROJECT_ID"]}-transcription-audio`);
-  const transcriptionFile = transcriptionBucket.file(name);
-  const transcriptionStream = transcriptionFile.createWriteStream();
+async function transcodePlayback(storage: Storage, filename: string): Promise<void> {
+  const sourceBucket = storage.bucket(`${process.env["PROJECT_ID"]}-raw-audio`);
+  const sourceFile = sourceBucket.file(filename);
 
-  return new Promise(resolve => {
+  const destBucket = storage.bucket(`${process.env["PROJECT_ID"]}-transcription-audio`);
+  const destFile = destBucket.file(filename);
+
+  return new Promise((resolve, reject) => {
     ffmpeg(sourceFile.createReadStream())
       .noVideo()
       .format("wav")
-      .audioCodec('pcm-mulaw')
-      .output(transcriptionStream, {end: true})
+      .output(destFile.createWriteStream(), {end: true})
       .on("end", () => resolve())
+      .on("error", err => reject(err))
       .run()
   });
 }
