cdec9e3db6cae9b23d09e4a887877f493c28d277
steven <github@stevenwaterman.uk>
2021-11-15 18:41:43 +0000

    Correctly calculate size from bitrate

--- functions/charge_credit/src/index.ts
+++ functions/charge_credit/src/index.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 import { Storage } from "@google-cloud/storage";
 
 const bitrate = 128;
+const bytesPerSecond = 128 / 8 * 1024;
 
 async function handleRequest(req: Request, res: Response) {
   const { user, transcript } = await utils.userTranscript.getAll(req, res, store);
@@ -11,17 +12,18 @@ async function handleRequest(req: Request, res: Response) {
   const filename = `${user.id}_${transcript.id}`;
   const bucket = storage.bucket(`${process.env["PROJECT_ID"]}-playback-audio`);
   const file = bucket.file(`${filename}.mp3`);
+
   const [metadata] = await file.getMetadata();
   const size = metadata.size;
-  const duration = size / bitrate;
-  console.log(size)
-  await transcript.doc.set({ audio: { duration }}, { merge: true });
+  const duration = size / bytesPerSecond;
+  const roundedDuration = Math.ceil(duration);
+  await transcript.doc.set({ audio: { duration: roundedDuration }}, { merge: true });
 
   const credit = user.data.get("secondsCredit");
 
-  if (credit >= duration) {
+  if (credit >= roundedDuration) {
     user.doc.update({
-      secondsCredit: admin.firestore.FieldValue.increment(-duration)
+      secondsCredit: admin.firestore.FieldValue.increment(-roundedDuration)
     });
     res.sendStatus(200);
   } else {
