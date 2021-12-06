ad679af848d4e7d0dc453986ecaa5d99408a271d
steven <github@stevenwaterman.uk>
2021-12-06 13:20:16 +0000

    Begin migration to sveltekit for static site generation

--- frontend/dev.sh
+++ frontend/dev.sh
@@ -1,4 +1,11 @@
+cd landing
+npm i
+npm run build
+cd ..
+rsync -a static/ public/
+
 ( cd dashboard; npm i; npm run dev ) &
 ( cd editor; npm i; npm run dev ) &
 ( cd editor; npm i; npm run dev-demo ) &
+
 serve -n -l 5000 public
---
+++ frontend/landing/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<style>
+	h1 {
+		width: 100%;
+	}
+</style>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>
+  Welcome to your new<br />SvelteKit app
+</h1>
