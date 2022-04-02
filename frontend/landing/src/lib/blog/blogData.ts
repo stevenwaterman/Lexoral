4e4c78aece7cf72aa63fd957fdd6ca469ffe8fdf
steven <github@stevenwaterman.uk>
2022-04-02 12:20:06 +0100

    Add images to speaking confidently blog post

--- frontend/landing/src/lib/blog/blogData.ts
+++ frontend/landing/src/lib/blog/blogData.ts
@@ -100,7 +100,7 @@ export const blogPosts = assertBlogPosts({
     author: "SteWaterman",
     title: "You can learn to speak with confidence",
     shortDescription: "Do the words 'public speaking' strike fear into your heart? I used to be the same. Used to.",
-    longDescription: "Do the words 'public speaking' strike fear into your heart? It doesn't come naturally to me either, but over time I've slowly got better at it. This post is all about the things that helped me, so you can learn too.",
+    longDescription: "Do the words 'public speaking' strike fear into your heart? It doesn't come naturally to me either, but over time I've slowly improved. This post is all about the things that helped me, so you can learn too.",
     date: new Date("2022-03-30T12:00:00Z"),
     featured: true,
     published: true
